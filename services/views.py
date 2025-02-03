6530b07ca82267a2a520e6c4d8d5c766b2db0ea0
Elmokhtar Ahmed <mokhtar.jo21@gmail.com>
2025-02-03 02:07:56 +0200

    add 2 services

--- services/views.py
+++ services/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-     return render(request, 'services/form.html')
+     return render(request, 'services/talb_fhas.html')
